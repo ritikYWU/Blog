00bf1f2d0e2ab9bedcef64bc776ad927a54e0f95
RitiKSIBJr <letter.ritik011@gmail.com>
2023-09-18 11:31:42 +0545

    create api fixed

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 
 from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 
 from .models import Blog
 from .serializers import BlogSerializer
@@ -18,16 +19,13 @@ def blog_list(request):
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def create(request):
+    serialized_data = BlogSerializer(data=request.data)
 
-    if request.method == 'POST':
-
-        serialized_data = BlogSerializer(data=request.data)
-
-        if serialized_data.is_valid():
-            serialized_data.save(request.user)
-            return Response('Created')
+    if serialized_data.is_valid():
+        serialized_data.save()
+        return Response(data={"status":status.HTTP_201_CREATED, "message":"Blog created successfully", "data":serialized_data.data})
         
-    return Response(data={'message':serialized_data.errors})
+    return Response(data={"status":status.HTTP_400_BAD_REQUEST, "message":serialized_data.errors})
 
 
 @api_view(['POST', 'PUT'])
